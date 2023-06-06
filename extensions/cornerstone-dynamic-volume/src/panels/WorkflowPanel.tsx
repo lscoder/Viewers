70b7ecfdf23e49acc534a764b7252baae4612596
Leonardo Campos <leonardoscampos@gmail.com>
2023-06-06 13:40:34 -0300

    StepProgressDropdown

--- extensions/cornerstone-dynamic-volume/src/panels/WorkflowPanel.tsx
+++ extensions/cornerstone-dynamic-volume/src/panels/WorkflowPanel.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useMemo, useCallback } from 'react';
+import { StepProgressDropdown } from '@ohif/ui';
 
 const styles = {
   panel: {
@@ -24,7 +25,7 @@ function WorkflowPanel({ servicesManager }) {
   const [stages, setStages] = useState(workflowStagesService.stages);
   const [activeStage, setActiveStage] = useState(workflowStagesService.activeStage);
 
-  const handleClick = useCallback((stage) => {
+  const handleStageSelected = useCallback((stage) => {
     workflowStagesService.setActiveStage(stage.id);
   }, [workflowStagesService]);
 
@@ -54,19 +55,42 @@ function WorkflowPanel({ servicesManager }) {
     return (
       <div
         key={stage.id}
-        onClick={() => handleClick(stage)}
+        onClick={() => handleStageSelected(stage)}
         style={ stage.id === activeStage?.id ? styles.listItemSelected : styles.listItem }
       >
         { stage.id === activeStage?.id && '[' } { stage.name } { stage.id === activeStage?.id && ']' }
       </div>
     );
-  })
+  });
+
+  const menuOptions = useMemo(() => (
+    stages.map(stage => (
+      {
+        label: stage.name,
+        value: stage.id,
+        icon: 'info',
+        info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      }
+    ))
+  ), [stages]);
+
+  const handleDropdownChange = ({ selectedOption }) => {
+    if (!selectedOption) {
+      return;
+    }
+
+    const stage = stages.find(stage => stage.id === selectedOption.value);
+    handleStageSelected(stage);
+  };
 
   return (
     <div data-cy={'workflow-panel'} style={styles.panel}>
       <div style={styles.title}>Workflow</div>
       <div style={styles.container}>
         { stagesContent }
+      </div>
+      <div style={{ backgroundColor: '#041c4a', padding: '10px 5px' }}>
+        <StepProgressDropdown id="options" options={menuOptions} value={activeStage?.id} onChange={handleDropdownChange}></StepProgressDropdown>
       </div>
     </div>
   );
